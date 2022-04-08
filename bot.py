7cc7cf09d42e765ba3208ac857c353ca6914396f
落月 <101474180+fallmoonstar@users.noreply.github.com>
2022-04-08 21:32:10 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands
 import json
 import random
+import os
 
 with open("setting.json",mode="r",encoding="utf8") as jfile:
     jdata = json.load(jfile)
@@ -12,7 +13,7 @@ with open("setting.json",mode="r",encoding="utf8") as jfile:
 intents = discord.Intents.default()
 intents.members = True
 
-
+print("Hello world")
 #BOT指令為^
 bot = commands.Bot(command_prefix='^',intents = intents)
 #上線後回傳到黑窗
@@ -23,34 +24,41 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(int(jdata["Welcome_Channel"]))
-    await channel.send(f"@everyone {member} join!")
+    await channel.send(f"@here {member} join!")
     print(f"{member} join!")
 
 @bot.event
 async def on_member_remove(member):
     channel = bot.get_channel(int(jdata["Leave_Channel"]))
-    await channel.send(f"@everyone {member} leave!")
+    await channel.send(f"@here {member} leave!")
     print(f"{member} leave!")
-#  BOT^指令
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f"{round(bot.latency*1000)} (ms)")
 
 @bot.command()
-async def number3(ctx):
+async def load(ctx, extension):
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.send(f'Loaded {extension} done.')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cmds.{extension}')
+    await ctx.send(f'Un - Loaded {extension} done.')
+
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(f'cmds.{extension}')
+    await ctx.send(f'Re - Loaded {extension} done.')
+
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cmds.{filename[:-3]}')
+
+@bot.command()
+async def 探路者(ctx):
     random_N3 = random.choice(jdata["N3"])
     N3 = str(random_N3)
     await ctx.send(N3)
 
-@bot.command()
-async def 早安(ctx):
-    await ctx.send(f"早安")
-
-@bot.command()
-async def 圖片(ctx):
-    random_pic = random.choice(jdata["pic"])
-    pic = discord.File(random_pic)
-    await ctx.send(file= pic)
-
 #RUNbot
-bot.run(jdata["TOKEN"])
+if __name__ == "__main__":
+
+    bot.run(jdata["TOKEN"])
